9d6e9629a08523013b4b1120ee334071d1d3a0f8
Brett Boston <bboston7@gmail.com>
2014-03-31 15:17:54 -0700

    Completed get-assignment

--- assign.rkt
+++ assign.rkt
@@ -6,19 +6,36 @@
 (define SECONDS_IN_WEEK (* 60 60 24 7))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Public functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (get-assignment)
-  (let* ([week (floor (/ (- (current-seconds) DATE_S) SECONDS_IN_WEEK))]
-         [rotations (modulo week (length PEOPLE))])
-    null))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Private functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+#|
+Rotates a list times times
+|#
 (: rotate ((Listof Any) Nonnegative-Integer -> (Listof Any)))
 (define (rotate lst times)
   (let-values ([(tail head) (split-at lst (- (length lst) times))])
     (append head tail)))
+
+#|
+Zips two lists together into a list of pairs
+|#
+(: zip ((Listof Any) (Listof Any) -> (Listof (Pair Any Any))))
+(define (zip lst1 lst2)
+  (for/list ([i lst1]
+             [j lst2])
+    (cons i j)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Public functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#|
+Get the weeks chore assignment
+|#
+(define (get-assignment)
+  (let* ([week (floor (/ (- (current-seconds) DATE_S) SECONDS_IN_WEEK))]
+         [rotations (modulo week (length PEOPLE))]
+         [names (rotate PEOPLE rotations)])
+    (zip CHORES names)))
+
